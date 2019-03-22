694546c19b9876097d67e9720b72f818f6cd2ed6
Evgeny Matviyenko <evgeny.matviyenko@gmail.com>
2019-03-22 12:01:04 +0200

    Add mock view items

---
+++ HabitApp/ViewItem.swift
@@ -0,0 +1,75 @@
+//
+//  ViewItem.swift
+//  HabitApp
+//
+//  Created by Евгений Матвиенко on 3/22/19.
+//  Copyright © 2019 Tolgahan Arıkan. All rights reserved.
+//
+
+import Foundation
+
+struct ViewItem {
+    struct Celebrity {
+        var name: String
+        var imageName: String
+        var link: URL
+    }
+
+    enum DescriptionItem {
+        case text(String)
+        case links(title: String?, links: [URL])
+        case image(name: String)
+        case celebrities(title: String, celebrities: [Celebrity])
+    }
+
+    var name: String
+    var imageName: String
+    var shortDescription: String
+    var description: [DescriptionItem]
+}
+
+extension ViewItem {
+    static func mockItems() -> [ViewItem] {
+        return [
+            ViewItem(
+                name: "Healthy Guy",
+                imageName: "healthy_guy.jpg",
+                shortDescription: "Healthy Guy is even more healthy",
+                description: [
+                    .text(mockText),
+                    .image(name: "healthy_guy.jpg"),
+                    .links(title: "Links", links: [URL(string: "https://google.com")!]),
+                    .celebrities(title: "Healthy guys", celebrities: [Celebrity(name: "Johnny Cage", imageName: "johnny_cage.jpg", link: URL(string: "https://google.com")!)])
+                ]
+            ),
+            ViewItem(
+                name: "Healthy Guy",
+                imageName: "healthy_guy.jpg",
+                shortDescription: "Healthy Guy is even more healthy",
+                description: [
+                    .text(mockText),
+                    .image(name: "healthy_guy.jpg"),
+                    .links(title: "Links", links: [URL(string: "https://google.com")!]),
+                    .celebrities(title: "Healthy guys", celebrities: [Celebrity(name: "Johnny Cage", imageName: "johnny_cage.jpg", link: URL(string: "https://google.com")!)])
+                ]
+            ),
+            ViewItem(
+                name: "Healthy Guy",
+                imageName: "healthy_guy.jpg",
+                shortDescription: "Healthy Guy is even more healthy",
+                description: [
+                    .text(mockText),
+                    .image(name: "healthy_guy.jpg"),
+                    .links(title: "Links", links: [URL(string: "https://google.com")!]),
+                    .celebrities(title: "Healthy guys", celebrities: [Celebrity(name: "Johnny Cage", imageName: "johnny_cage.jpg", link: URL(string: "https://google.com")!)])
+                ]
+            )
+        ]
+    }
+}
+
+private let mockText = """
+# Welcome to StackEdit!
+
+Hi! I'm your first Markdown file in **StackEdit**. If you want to learn about StackEdit, you can read me. If you want to play with Markdown, you can edit me. Once you have finished with me, you can create new files by opening the **file explorer** on the left corner of the navigation bar
+"""
